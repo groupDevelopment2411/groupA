72c71df661782d9086cb35ea6bca008a5e222e81
ryoichirosaito <ryoichiro.saito@gmail.com>
2025-02-28 20:35:14 +0900

    20250228　005006ファイル更新

--- src/main/java/com/example/demo/update/UpdateMapper.java
+++ src/main/java/com/example/demo/update/UpdateMapper.java
@@ -1,5 +1,17 @@
 package com.example.demo.update;
 
-public class UpdateMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+
+
+@Mapper
+public interface UpdateMapper {
+	
+	@Select("SELECT * FROM employee WHERE id = #{id}")
+	List<Update> findUserById(int id);
+
+	
 }
--- src/main/java/com/example/demo/update/UpdateService.java
+++ src/main/java/com/example/demo/update/UpdateService.java
@@ -1,5 +1,19 @@
 package com.example.demo.update;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UpdateService {
 
+	@Autowired
+	private UpdateMapper mapper;
+	
+	public List<Update> findUserById(int id){
+		return mapper.findUserById(id);
+	}
+	
 }
